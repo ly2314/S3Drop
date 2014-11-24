1242639757d5443605df3f18be555d264fe78e87
Ting-Yuan Hsia <ly2314@gmail.com>
2014-11-24 19:50:49 +0800

    add aws account config sample

---
+++ app/views/common/aws_api_credential.sample.php
@@ -0,0 +1,16 @@
+<?php
+/*
+    Code by ly2314
+    https://www.ly2314.cc
+*/
+    return array(
+        'includes' => array('_aws'),
+        'services' => array(
+            'default_settings' => array(
+                'params' => array(
+                    'key'    => 'ENTER_YOUR_IAM_KEY_HERE',
+                    'secret' => 'ENTER_YOUR_IAM_SECRET_HERE',
+                )
+            )
+        ));
+?>
--- app/views/common/config.php
+++ app/views/common/config.php
@@ -4,6 +4,8 @@
     https://www.ly2314.cc
 */
     $config_BUCKET = 'sylab-s3drop';
+    $config_BUCKET03 = 'sylab-s3drop-03';
+    $config_BUCKET30 = 'sylab-s3drop-30';
     $ffmpeg_path = 'C:\ffmpeg\bin\ffmpeg.exe';
     $token_server = 'http://140.116.221.81/Oauth/public/index.php/';
 ?>
