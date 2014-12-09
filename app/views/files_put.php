3861b7817d8278511e1eadc6dce0c9a05247dab8
Ting-Yuan Hsia <ly2314@gmail.com>
2014-12-09 22:52:36 +0800

    create folder while uploading files if folder not exists

--- app/views/files_put.php
+++ app/views/files_put.php
@@ -7,6 +7,21 @@
 
     $request = Request::instance();
     $content = $request->getContent();
+    $temp_pwd = $pwd;
+    if (!endsWith($pwd, '/'))
+    {
+        $pwd = substr($pwd, 0, strrpos($pwd, '/') + 1);
+    }
+    while ($pwd != '/')
+    {
+        $result = $s3client->putObject(array(
+            'Bucket'       => $bucket_name,
+            'Key'          => $username.$pwd,
+            'Body'         => '',
+        ));
+        $pwd = substr($pwd, 0, strrpos($pwd, '/', -2) + 1);
+    }
+    $pwd = $temp_pwd;
     if (isset($expires))
     {
         $result = $s3client->putObject(array(
